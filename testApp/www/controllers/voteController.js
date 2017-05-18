01184eeb0e2117fbc51027ff6ba83c98c71cd076
remy peyre <remy.peyre05@gmail.com>
2017-05-19 00:48:41 +0200

    custon view home and map

--- testApp/www/controllers/voteController.js
+++ testApp/www/controllers/voteController.js
@@ -1,5 +1,3 @@
-
-
 (function () {
     'use strict';
 
@@ -61,62 +59,3 @@
     };
 }());
 
-
-
-
-
-
-
-/*
-angular.module('testApp', ['ionic'])
-    .controller('VoteCtrl',function($scope, $ionicPopup, $timeout) {
-
-        // Triggered on a button click, or some other target
-        $scope.showPopup = function() {
-            $scope.data = {}
-
-            // An elaborate, custom popup
-            var myPopup = $ionicPopup.show({
-                template: '<input type="password" ng-model="data.wifi">',
-                title: 'Enter Wi-Fi Password',
-                subTitle: 'Please use normal things',
-                scope: $scope,
-                buttons: [
-                    { text: 'Cancel' },
-                    {
-                        text: '<b>Save</b>',
-                        type: 'button-positive',
-                        onTap: function(e) {
-                            if (!$scope.data.wifi) {
-                                //don't allow the user to close unless he enters wifi password
-                                e.preventDefault();
-                            } else {
-                                return $scope.data.wifi;
-                            }
-                        }
-                    },
-                ]
-            });
-            myPopup.then(function(res) {
-                console.log('Tapped!', res);
-            });
-            $timeout(function() {
-                myPopup.close(); //close the popup after 3 seconds for some reason
-            }, 3000);
-        };
-        // A confirm dialog
-        $scope.voteConfirm = function() {
-            var confirmPopup = $ionicPopup.confirm({
-                title: 'Consume Ice Cream',
-                template: 'Are you sure you want to eat this ice cream?'
-            });
-            confirmPopup.then(function(res) {
-                if(res) {
-                    console.log('You are sure');
-                } else {
-                    console.log('You are not sure');
-                }
-            });
-        };
-
-    });*/
